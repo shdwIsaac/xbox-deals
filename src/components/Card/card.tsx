4614faff9dc2805aebc65ee4f14a1463a1b6f6f7
DenisPoleshchuk <denis.poleschyk@gmail.com>
2024-10-10 17:56:53 +0300

    improve

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -1,4 +1,12 @@
-import React, { type FC } from 'react'
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
+/* eslint-disable @typescript-eslint/space-infix-ops */
+/* eslint-disable @typescript-eslint/prefer-nullish-coalescing */
+/* eslint-disable @typescript-eslint/no-floating-promises */
+/* eslint-disable @typescript-eslint/prefer-ts-expect-error */
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable react/jsx-no-undef */
+/* eslint-disable @typescript-eslint/no-misused-promises */
+import React, { useState, type FC } from 'react'
 import {
   Card,
   CardFooter,
@@ -14,7 +22,7 @@ import {
 } from '@nextui-org/react'
 import styles from './card.module.css'
 import { useBreakpoint } from '../../breakpoints'
-
+import { getDeal, type IGameDeals } from '../../utils/api'
 interface IProduct {
   logo: string
   name: string
@@ -25,8 +33,24 @@ interface IProduct {
 }
 export const GameCard: FC<IProduct> = (product) => {
   const { isScreenS } = useBreakpoint()
-  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure()
 
+  const [gameDeals, setGameDeals] = useState<IGameDeals>()
+  const [selectedCountry, setSelectedCountry] = useState<string>(product.country)
+  console.debug({ selectedCountry })
+  const processDeal = async (): Promise<void> => {
+    getDeal(product.id).then(resp => { setGameDeals(resp) })
+  }
+
+  const onChangeSelect = (e: any): void => {
+    console.debug('onChangeSelect', e.target.value)
+    setSelectedCountry(e.target.value)
+  }
+
+  const { isOpen, onOpen, onClose, onOpenChange } = useDisclosure({
+    onOpen: processDeal
+  })
+
+  const priceSelectedCountry = selectedCountry ? gameDeals?.deals?.find(d => d.country === selectedCountry)?.priceRub : undefined
   return (
     <div className='ps-1 pe-1'>
       <Card isFooterBlurred radius="lg" className={`border-none ${styles.my_card_wrapp}`}>
@@ -54,18 +78,18 @@ export const GameCard: FC<IProduct> = (product) => {
       </Card>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange} onClose={onClose}>
         <ModalContent>
-        <ModalHeader />
+          <ModalHeader />
           <ModalBody>
             <img src={product.logo} alt={product.name} className={styles.img_modal} />
             <b>{product.name}</b>
             <div className='flex items-center pb-4'>
-            <Select defaultSelectedKeys={[product.country]}>
-              <SelectItem key={product.country} value={product.country}>
-                {product.country}
-              </SelectItem>
-            </Select>
-            <span className={styles.price} >{product.minPrice} RUB</span>
+              <Select defaultSelectedKeys={[product.country]} onChange={onChangeSelect} selectedKeys={[selectedCountry]}>
+              {/* @ts-ignore */}
+              {gameDeals?.deals.map((element, index) => (<SelectItem key={element.country} value={element.country}>{element.country}</SelectItem>))}
+              </Select>
+              <span className={styles.price} >{priceSelectedCountry || product.minPrice} RUB</span>
             </div>
+
           </ModalBody>
         </ModalContent>
       </Modal>
